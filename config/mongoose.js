91f38a91ec7de65f40c6ec6137f9b3aebcb43559
dhwanishah9927 <83601581+dhwanishah9927@users.noreply.github.com>
2024-04-11 19:53:31 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 // const DB = 'mongodb://localhost:27017/habit-tracker';
-const DB = 'mongodb+srv://dhwanishah2799:<password>@cluster0.ffbyepm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+//const DB = 'mongodb+srv://dhwanishah2799:<password>@cluster0.ffbyepm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DB = 'mongodb+srv://ajaykumar:FLrbtKo1UydPJ3FW@auth-project.p6afx7q.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connection successful!');
